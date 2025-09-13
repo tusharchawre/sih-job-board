c59de346ae4dacb8cfbd306791d15b58a03aef01
tusharchawre <tusharchawre0240@gmail.com>
2025-09-13 21:50:32 +0530

    init

---
+++ apps/backend/index.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@repo/db/client";
+import express from "express";
+
+const app = express()
+
+
+app.listen(8080, () => {
+  console.log("Server is running on port 3000");
+});
