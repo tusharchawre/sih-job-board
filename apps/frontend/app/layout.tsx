2ba55ad7bd92c725e73005fc661eda4d569b7297
sudhanshu-jawanjal <sudhanshujawanjal007@gmail.com>
2025-09-14 20:04:23 +0530

    add: landing page created

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import { Navbar } from "@/components/landingPage/Navbar";
+import { Footer } from "@/components/landingPage/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className}`}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
