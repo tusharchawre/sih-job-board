4a6673a2a56ac7311abb45412b4f6bd3eafedc5f
tusharchawre <tusharchawre0240@gmail.com>
2025-09-14 18:39:11 +0530

    add: routes

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <div className="p-4  ">
+      <div className="p-4">
         <h1 className="text-red-500 ">test tailwind</h1>
       </div>
     </>
